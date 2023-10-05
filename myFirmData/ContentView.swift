bb61051b0640eb2669ea17b2309a28ce0e725269
myFirmData <38827942+myFirmData@users.noreply.github.com>
2023-10-05 16:29:06 -0400

    Initial Commit

---
+++ myFirmData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myFirmData
+//
+//  Created by RAJU PENUMATCHA on 10/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myFirmData/myFirmDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFirmDataApp.swift
+//  myFirmData
+//
+//  Created by RAJU PENUMATCHA on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct myFirmDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
